dc36caced33c800ef8998393262d1874c58b3373
admin <509194515@qq.com>
2021-08-25 22:04:43 +0800

    2021年8月25日22:04:39

--- 第12章 处理Excel电子表格　/13.实践项目/5.电子表格到文本文件.py
+++ 第12章 处理Excel电子表格　/13.实践项目/5.电子表格到文本文件.py
@@ -1,3 +1,20 @@
 import openpyxl, os
+from openpyxl.utils import get_column_letter
 
+os.chdir('C:\\Users\\admin\\PycharmProjects\\ATBSWP\\第12章 处理Excel电子表格　\\13.实践项目')
 
+wb = openpyxl.load_workbook('txt2excel.xlsx')
+sheet = wb.active
+
+row = sheet.max_row
+column = sheet.max_column
+
+n = 1
+for j in range(1, column + 1):
+    with open('excel2txt%s.txt' % n, 'w') as f:
+        for i in range(1, row + 1):
+            value = sheet[get_column_letter(j) + str(i)].value
+            if value is not None:
+                f.write(value)
+                f.write('\n')
+    n += 1
---
+++ 第17章 操作图像/2.用Pillow操作图像.py
@@ -0,0 +1,4 @@
+import os
+os.chdir('C:\\Users\\admin\\PycharmProjects\\ATBSWP\\第17章 操作图像')
+os.getcwd()
+os.chdir('zophie.png')
