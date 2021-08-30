23f0a339428492ec00d5a889cd9ab0412b57d886
admin <509194515@qq.com>
2021-08-30 22:06:32 +0800

    2021年8月30日22:06:27

--- 第15章 保持时间、计划任务和启动程序/6.多线程.py
+++ 第15章 保持时间、计划任务和启动程序/6.多线程.py
@@ -9,3 +9,9 @@ while datetime.datetime.now() < startTime:
     time.sleep(1)
 
 print('Program now starting on Halloween 2029')
+
+# 这段代码指定 2029 年 10 月 31 日作为开始时间，不断调用 time.sleep(1)，直到开
+# 始时间。在等待 time.sleep()的循环调用完成时，程序不能做任何事情，它只是坐在那
+# 里，直到 2029 年万圣节。这是因为 Python 程序在默认情况下，只有一个执行线程。
+
+
